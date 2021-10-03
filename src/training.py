0b9998d87ce1a587c8f8db673c1a4897a637c0e6
Abhishek Gupta <guptabhishek8@gmail.com>
2021-10-03 19:47:08 +0530

    training stage done

--- src/training.py
+++ src/training.py
@@ -1,5 +1,6 @@
-from src.utils.common import read_config
-from src.utils.data_mgmt import get_data
+from utils.common import read_config
+from utils.data_mgmt import get_data
+from utils.model import create_model
 import argparse
 
 
@@ -7,8 +8,21 @@ def training(config_path):
     config = read_config(config_path)
 
     validation_datasize = config["param"]["validation_datasize"]
-    (X_train_full, y_train_full), (X_valid, y_valid), (X_test,
-                                                       y_test) = get_data(validation_datasize)
+    (X_train, y_train), (X_valid, y_valid), (X_test,
+                                             y_test) = get_data(validation_datasize)
+
+    LOSS_FUNCTION = config["param"]["loss_function"]
+    OPTIMIZER = config["param"]["optimizer"]
+    METRICS = config["param"]["metrics"]
+    NUM_CLASSES = config["param"]["num_classes"]
+
+    model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+
+    EPOCH = config["param"]["epochs"]
+    VALIDATION = (X_valid, y_valid)
+
+    history = model.fit(
+        X_train, y_train, epochs=EPOCH, validation_data=VALIDATION)
 
 
 if __name__ == '__main__':
@@ -18,4 +32,4 @@ if __name__ == '__main__':
 
     parsed_args = args.parse_args()
 
-    training(config_path) = parsed_args.config
+    training(config_path=parsed_args.config)
