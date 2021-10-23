97e5223cadae0c1a48f40060129dc26d3c59ff41
Abhishek Gupta <guptabhishek8@gmail.com>
2021-10-23 18:14:53 +0530

    comments added

--- src/training.py
+++ src/training.py
@@ -3,7 +3,7 @@ from utils.common import read_config
 from utils.data_mgmt import get_data
 from utils.model import create_model, save_model
 from utils.save_plot import save_plot
-from src.utils.callbacks import get_callbacks
+from utils.callbacks import get_callbacks
 import argparse
 
 
@@ -11,7 +11,8 @@ def training(config_path):
     config = read_config(config_path)
 
     validation_datasize = config["params"]["validation_datasize"]
-    (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
+    (X_train, y_train), (X_valid, y_valid), (X_test,
+                                             y_test) = get_data(validation_datasize)
 
     LOSS_FUNCTION = config["params"]["loss_function"]
     OPTIMIZER = config["params"]["optimizer"]
@@ -26,7 +27,7 @@ def training(config_path):
     CALLBACK_LIST = get_callbacks(config, X_train)
 
     history = model.fit(X_train, y_train, epochs=EPOCHS,
-                        validation_data=VALIDATION_SET, callbacks= CALLBACK_LIST)
+                        validation_data=VALIDATION_SET, callbacks=CALLBACK_LIST)
 
     artifacts_dir = config["artifacts"]["artifacts_dir"]
     model_dir = config["artifacts"]["model_dir"]
@@ -38,7 +39,7 @@ def training(config_path):
 
     save_model(model, model_name, model_dir_path)
 
-    #Save Plot
+    # Save Plot
     plot_dir = config["artifacts"]["plot_dir"]
     plot_dir_path = os.path.join(artifacts_dir, plot_dir)
     os.makedirs(plot_dir_path, exist_ok=True)
